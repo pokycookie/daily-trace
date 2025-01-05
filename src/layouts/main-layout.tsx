0191b5c759961396a1850133a2271b1ab53adb02
pokycookie <0cookieboy0@gmail.com>
2025-01-05 17:22:17 +0900

    chore: mui theme 설정

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,5 +1,18 @@
+import { useThemeStore } from '@/stores'
+import { darkTheme, lightTheme } from '@/themes/theme'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import { useMemo } from 'react'
 import { Outlet } from 'react-router-dom'
 
 export default function MainLayout() {
-  return <Outlet />
+  const mode = useThemeStore((state) => state.mode)
+
+  const theme = useMemo(() => createTheme(mode === 'light' ? lightTheme() : darkTheme()), [mode])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Outlet />
+    </ThemeProvider>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import router from '@/routes/router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import '@/global.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
