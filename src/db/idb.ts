d19458019da9d653adddd9f22bd0b8888990e03e
pokycookie <0cookieboy0@gmail.com>
2025-01-05 17:43:51 +0900

    chore: indexedDB 세팅

---
+++ src/db/idb.ts
@@ -0,0 +1,40 @@
+import Dexie, { EntityTable } from 'dexie'
+
+export interface ITrace {
+  id: number
+  title: string
+  content: string
+  date: Date
+}
+
+export interface ITraceAttribute {
+  id: number
+  traceId: number
+  type: string
+  attribute: string
+}
+
+export interface ITraceTag {
+  id: number
+  traceId: number
+  tagId: number
+}
+
+export interface ITag {
+  id: number
+  tag: string
+}
+
+export const db = new Dexie('trace') as Dexie & {
+  traces: EntityTable<ITrace, 'id'>
+  traceAttributes: EntityTable<ITraceAttribute, 'id'>
+  traceTags: EntityTable<ITraceTag, 'id'>
+  tags: EntityTable<ITag, 'id'>
+}
+
+db.version(1).stores({
+  traces: '++id, date',
+  traceAttributes: '++id',
+  traceTags: '++id',
+  tags: '++id, tag',
+})
